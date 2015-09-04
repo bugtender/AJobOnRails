5398de46a0dc1ca40274d1a095eb91ff6feee20e
Vincent Lin <viflin@gmail.com>
2015-09-04 12:14:35 +0800

    fix timezone

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Jobonrails
   class Application < Rails::Application
+    config.time_zone = "Taipei"
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
